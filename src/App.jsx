8ae706861fd1eaa48428ab10ae43d25f8ad40f98
Fred Pene <fredpene@hotmail.co.nz>
2022-11-30 08:51:02 +1300

    fix div indentation

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,21 @@ import './App.css'
 function App() {
   return (
     <>
-      <div className="App">
-        <h1>Pokedex</h1>
-        <div className="poke-container" id="poke-container"></div>
+      <h1>Pokedex</h1>
+      <div className="poke-container" id="pokemon-container">
         <div className="pokemon">
           <div className="img-container">
             <img
               src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"
               alt="bulbasaur"
             />
-            <div className="info">
-              <span className="number">#001</span>
-              <h3 className="name">Bulbasaur</h3>
-              <small className="type">
-                Type: <span>grass</span>
-              </small>
-            </div>
+          </div>
+          <div className="info">
+            <span className="number">#001</span>
+            <h3 className="name">Bulbasaur</h3>
+            <small className="type">
+              Type: <span>grass</span>
+            </small>
           </div>
         </div>
         <div className="pokemon">
@@ -28,13 +27,13 @@ function App() {
               src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png"
               alt="bulbasaur"
             />
-            <div className="info">
-              <span className="number">#002</span>
-              <h3 className="name">Ivysaur</h3>
-              <small className="type">
-                Type: <span>grass</span>
-              </small>
-            </div>
+          </div>
+          <div className="info">
+            <span className="number">#002</span>
+            <h3 className="name">Ivysaur</h3>
+            <small className="type">
+              Type: <span>grass</span>
+            </small>
           </div>
         </div>
         <div className="pokemon">
@@ -43,13 +42,13 @@ function App() {
               src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png"
               alt="bulbasaur"
             />
-            <div className="info">
-              <span className="number">#003</span>
-              <h3 className="name">Venusaur</h3>
-              <small className="type">
-                Type: <span>grass</span>
-              </small>
-            </div>
+          </div>
+          <div className="info">
+            <span className="number">#003</span>
+            <h3 className="name">Venusaur</h3>
+            <small className="type">
+              Type: <span>grass</span>
+            </small>
           </div>
         </div>
       </div>
