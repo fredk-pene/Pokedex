17692691a05c285d98c9e045d61914e8c1c823ab
Fred Pene <fredpene@hotmail.co.nz>
2022-12-01 13:36:53 +1300

    connect to api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const [pokeData, setPokeData] = useState([])
+  const [load, setLoad] = useState('https://pokeapi.co/api/v2/pokemon/?limit=20')
+
+  const pokeCount = 150
+
+  const fetchPokemon = async () => {
+    for (let i = 1; i <= pokeCount; i++) {
+      await getPokemon(i)
+    }
+  }
+
+  const getPokemon = async (id) => {
+    // const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const res = await fetch(load)
+    const data = await res.json()
+    createPokeCard(data)
+  }
+
+  let createPokeCard = (pokemon) => {
+    console.log(pokemon)
+  }
+  console.log(createPokeCard.name)
+
+  useEffect(() => {
+    getPokemon()
+  }, [])
+
+  fetchPokemon()
+
   return (
     <>
       <h1>Pokedex</h1>
@@ -21,7 +50,7 @@ function App() {
             </small>
           </div>
         </div>
-        <div className="pokemon">
+        {/* <div className="pokemon">
           <div className="img-container">
             <img
               src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png"
@@ -50,7 +79,7 @@ function App() {
               Type: <span>grass</span>
             </small>
           </div>
-        </div>
+        </div> */}
       </div>
     </>
   )
