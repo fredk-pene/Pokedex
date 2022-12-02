245e1be1344ee2210cf3cf1d04fcd7cdb1fb46b7
Fred Pene <fredpene@hotmail.co.nz>
2022-12-02 14:11:25 +1300

    add background color for individual types

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function App() {
 
   return (
     <>
-      <h1>Pokedex</h1>
+      <h1 className="title">Pokedex</h1>
       <div className="poke-container" id="pokemon-container">
         {pokeData.map((pokemon, index) => (
           <PokemonCard
@@ -50,63 +50,3 @@ function App() {
 }
 
 export default App
-
-// const pokeCount = 150
-
-{
-  /* <div className="pokemon">
-  <div className="img-container">
-    <img
-      src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"
-      alt="bulbasaur"
-    />
-  </div>
-  <div className="info">
-    <span className="number">#001</span>
-    <h3 className="name">Bulbasaur</h3>
-    <small className="type">
-      Type: <span>grass</span>
-    </small>
-  </div>
-</div> */
-}
-{
-  /* <button className='load-more'>Load More</button> */
-}
-{
-  /* <div className="pokemon">
-  <div className="img-container">
-  <img
-  src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png"
-  alt="bulbasaur"
-  />
-  </div>
-  <div className="info">
-  <span className="number">#002</span>
-  <h3 className="name">Ivysaur</h3>
-  <small className="type">
-  Type: <span>grass</span>
-  </small>
-  </div>
-  </div>
-  <div className="pokemon">
-  <div className="img-container">
-  <img
-  src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png"
-  alt="bulbasaur"
-  />
-  </div>
-  <div className="info">
-  <span className="number">#003</span>
-  <h3 className="name">Venusaur</h3>
-  <small className="type">
-  Type: <span>grass</span>
-  </small>
-  </div>
-</div> */
-}
-// const fetchPokemon = async () => {
-//   for (let i = 0; i <= pokeCount; i++) {
-//     await getPokemon(i)
-//   }
-// }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -4,9 +4,12 @@ const pokemonCard = ({ id, name, image, type }) => {
   const pokeName = name.charAt(0).toUpperCase() + name.slice(1)
   const pokeType = type.charAt(0).toUpperCase() + type.slice(1)
   const pokeId = id.toString().padStart(3, '0')
+
+  const cardColor = `pokemon ${type}`
+
   return (
     <div>
-      <div className="pokemon">
+      <div className={cardColor}>
         <div className="img-container">
           <img src={image} alt={name} />
         </div>
