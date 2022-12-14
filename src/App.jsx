75f62cf8286e7c38e29e1540a134ce8524928f70
Fred Pene <fredpene@hotmail.co.nz>
2022-12-14 13:03:57 +1300

    add functions to numerically order data

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,43 @@ import PokemonCard from './components/PokemonCard'
 
 function App() {
   const [pokeData, setPokeData] = useState([])
+  const [pokeDataIds, setPokeDataIds] = useState(new Set())
   const [loadPoke, setLoadPoke] = useState(
     'https://pokeapi.co/api/v2/pokemon/?&limit=21'
   )
 
   const getPokemon = async () => {
-    const res = await fetch(loadPoke)
-    const data = await res.json()
+    const res = await fetch(loadPoke);
+    const data = await res.json();
 
-    setLoadPoke(data.next)
+    setLoadPoke(data.next);
 
-    function createPokeObj(result) {
-      result.forEach(async (pokemon) => {
-        const res = await fetch(
-          `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
-        )
-        const data = await res.json()
-        setPokeData((currentList) => [...currentList, data])
-      })
+    // Create a new Set to store unique pokemon IDs
+    const uniqueIds = new Set();
+
+    // Use a for-of loop to iterate over the pokemon data
+    // and create a new list of pokemon objects with only unique IDs
+    const newPokeData = [];
+    for (const pokemon of data.results) {
+      const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
+      const pokeData = await res.json();
+
+      // Add the pokemon to the new list if its ID is not in the Set
+      if (!uniqueIds.has(pokeData.id)) {
+        newPokeData.push(pokeData);
+        uniqueIds.add(pokeData.id);
+      }
     }
-    createPokeObj(data.results)
-     console.log(data.results)
-  }
+
+    // Use the spread operator to add the new pokemon to the existing list
+    setPokeData([...pokeData, ...newPokeData]);
+    setPokeDataIds(uniqueIds);
+  };
 
   useEffect(() => {
-    getPokemon()
-  }, [])
+    getPokemon();
+  }, []);
+
 
   return (
     <>
@@ -37,13 +48,13 @@ function App() {
       <div className="poke-container" id="pokemon-container">
         {pokeData.map((pokemon, index) => (
           <PokemonCard
-          id={pokemon.id}
-          image={pokemon.sprites.other['official-artwork'].front_default}
-          name={pokemon.name}
-          type={pokemon.types[0].type.name}
-          key={index}
+            id={pokemon.id}
+            image={pokemon.sprites.other['official-artwork'].front_default}
+            name={pokemon.name}
+            type={pokemon.types[0].type.name}
+            key={index}
           />
-          ))}
+        ))}
       </div>
       {/* <button class="btn" onClick={() => getPokemon()}>
         Load More
